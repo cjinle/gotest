fddb27b8ac0367a8fdd95171ba478e224898e8f4
cjinle <cjinle@gmail.com>
2020-10-30 17:56:45 +0800

    main3

--- client/client.go
+++ client/client.go
@@ -33,36 +33,6 @@ func GetGameInfo(mid uint32) {
 
 }
 
-func GetGameInfo2(mid uint32) {
-	c, _ := net.Dial("tcp", "192.168.100.107:8051")
-	defer c.Close()
-
-	buff := bytes.NewBuffer([]byte{})
-	binary.Write(buff, binary.BigEndian, uint32(20))
-	binary.Write(buff, binary.BigEndian, byte(0x20))
-	binary.Write(buff, binary.BigEndian, byte(0x21))
-	binary.Write(buff, binary.BigEndian, uint16(1))
-	binary.Write(buff, binary.BigEndian, byte(0x10))
-	// random := rand.Intn(1000)
-	random := 100
-	binary.Write(buff, binary.BigEndian, uint32(0x1004^random))
-	binary.Write(buff, binary.BigEndian, uint16(random))
-	binary.Write(buff, binary.BigEndian, byte(0))
-	binary.Write(buff, binary.BigEndian, uint32(mid))
-
-	fmt.Printf("%s", hex.Dump(buff.Bytes()))
-	
-	n, err := c.Write(buff.Bytes())
-	fmt.Println(n, err)
-
-	rb := make([]byte, 4096)
-	// rb := []byte{}
-	if _, err := c.Read(rb); err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println(rb)
-}
-
 
 func DataPack(buff *bytes.Buffer) []byte {
 	retBuff := bytes.NewBuffer([]byte{})
