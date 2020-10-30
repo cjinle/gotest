86551ef47944d4d93acde6ad2ffebe620fb60c09
cjinle <cjinle@gmail.com>
2020-10-30 18:36:47 +0800

    client

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package client
 import (
 	"bytes"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"encoding/hex"
@@ -30,21 +31,39 @@ func GetGameInfo(mid uint32) {
 	data := rb[16:recvLen]
 	fmt.Printf("%s", hex.Dump(data))
 
+	buff = bytes.NewBuffer(data)
 
+	v := DataUnpack(bytes.NewBuffer(rb[16:recvLen]))
+	fmt.Println(v)
+
+}
+
+func DataUnpack(buff *bytes.Buffer) interface{} {
+	var ret, dataLen int32
+	binary.Read(buff, binary.BigEndian, &ret)
+	binary.Read(buff, binary.BigEndian, &dataLen)
+	info := make([]byte, dataLen-1)
+	binary.Read(buff, binary.BigEndian, &info)
+	var v interface{}
+	err := json.Unmarshal(info, &v)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return v
 }
 
 
 func DataPack(buff *bytes.Buffer) []byte {
-	retBuff := bytes.NewBuffer([]byte{})
-	binary.Write(retBuff, binary.BigEndian, uint32(buff.Len()+16))
-	binary.Write(retBuff, binary.BigEndian, byte(0x20))
-	binary.Write(retBuff, binary.BigEndian, byte(0x21))
-	binary.Write(retBuff, binary.BigEndian, uint16(1))
-	binary.Write(retBuff, binary.BigEndian, byte(0x10))
+	ret := bytes.NewBuffer([]byte{})
+	binary.Write(ret, binary.BigEndian, uint32(buff.Len()+16))
+	binary.Write(ret, binary.BigEndian, byte(0x20))
+	binary.Write(ret, binary.BigEndian, byte(0x21))
+	binary.Write(ret, binary.BigEndian, uint16(1))
+	binary.Write(ret, binary.BigEndian, byte(0x10))
 	random := rand.Intn(1000)
-	binary.Write(retBuff, binary.BigEndian, uint32(0x1004^random))
-	binary.Write(retBuff, binary.BigEndian, uint16(random))
-	binary.Write(retBuff, binary.BigEndian, byte(0))
-	binary.Write(retBuff, binary.BigEndian, buff.Bytes())
-	return retBuff.Bytes()
+	binary.Write(ret, binary.BigEndian, uint32(0x1004^random))
+	binary.Write(ret, binary.BigEndian, uint16(random))
+	binary.Write(ret, binary.BigEndian, byte(0))
+	binary.Write(ret, binary.BigEndian, buff.Bytes())
+	return ret.Bytes()
 }
