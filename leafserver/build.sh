be9d5ab6f03c77f50f3ac823c71dea5d1463ce0f
cjinle <cjinle@gmail.com>
2020-11-13 15:09:42 +0800

    leaf

--- leafserver/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# https://github.com/lonsharn/leafserver
-
-export GOPATH=`pwd`
-go build -o bin/server -gcflags "-N -l" src/server/main.go
-go build -o bin/client -gcflags "-N -l" src/test/client.go
-go build -o bin/test_glog -gcflags "-N -l" src/test/test_glog.go
