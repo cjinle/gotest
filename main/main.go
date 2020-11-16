103b6230b8ad9ec4c5500faef9d5779575b6bde1
cjinle <cjinle@gmail.com>
2020-11-16 10:51:17 +0800

    delete

--- main/main.go
+++ main/main.go
@@ -1,24 +1,17 @@
 package main
 
 import (
+	"encoding/binary"
 	"fmt"
-	"os"
 )
 
-type Fd interface{}
-
-func NewFile() (Fd, err error) {
-	return os.OpenFile("xx.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
-}
 
 func main() {
 	fmt.Println("test main starting ... ")
-	fd, _ = NewFile()
-	fd.logs("a")
-	fd.logs("b")
+	bs := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bs, 1)
+	fmt.Println(bs)
+	binary.BigEndian.PutUint32(bs, 1)
+	fmt.Println(bs)
 }
 
-func (fd *Fd) logs(str string) {
-	buf := []byte(str + "\n")
-	fd.Write(buf)
-}
