5f7aed338cfa2ada04039fa12240e0f20daf39bd
cjinle <cjinle@gmail.com>
2020-12-09 10:24:08 +0800

    xx

--- io/ioreader.go
+++ io/ioreader.go
@@ -2,16 +2,18 @@ package io
 
 import (
 	"log"
-	"errors"
+	// "errors"
+	"io"
 )
 
 type MyData struct {
-
+	r  *io.Reader
 }
 
 func (md *MyData) Read(p []byte) (int, error) {
 	log.Println(string(p))
 
-	return 0, errors.New("EOF")
+	return 0, nil
+	// return 0, errors.New("EOF")
 }
 
